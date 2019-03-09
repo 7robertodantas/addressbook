fb4fdadc644dbbc02ca8f13ccc695141a1725d8f
Roberto Dantas <7robertodantas@gmail.com>
2019-03-09 00:41:32 -0300

    Middleware to authenticate and verify resource ownership

---
+++ middleware/authenticate.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const debug = require('debug')('app:middleware')
+const auth = require('../db/auth')
+
+const authenticate = (req, res, next) => {
+  debug('Validating request token')
+
+  const token = req.headers.authorization
+  if (!token) {
+    const error = { message: 'Authorization header is required.' }
+    return res.status(401).send(error)
+  } else if (!token.startsWith('Bearer ')) {
+    const error = { message: 'Authorization header scheme is missing or the given scheme \
+     is not supported. Only Bearer scheme is supported.' }
+    return res.status(401).send(error)
+  }
+
+  const jwt = token.replace('Bearer ', '')
+  const user = auth.verify(jwt)
+  if (!user) {
+    const error = { message: 'Invalid or expired token.' }
+    return res.status(401).send(error)
+  }
+
+  req.user = user
+  return next()
+}
+
+module.exports = authenticate
---
+++ middleware/resourceOwner.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const debug = require('debug')('app:middleware')
+
+const resourceOwner = (req, res, next) => {
+  debug(`Validating if authenticated user matches userId ${req.params.userId} resource`)
+  if (!req.user) {
+    const error = { message: 'User is not authenticated.' }
+    return res.status(401).send(error)
+  } else if (req.params.userId && req.user.id !== req.params.userId) {
+    const error = { message: 'User does not have permission to access the given resource.' }
+    return res.status(403).send(error)
+  }
+  return next()
+}
+
+module.exports = resourceOwner
